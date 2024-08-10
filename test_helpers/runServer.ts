89381be6692c2ca6c57f094b1ed0032efec345b1
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-08-10 18:11:35 +0100

    refactor(send/tests): ensure `.end` is called when handlers terminate, `await sendFile`

--- test_helpers/runServer.ts
+++ test_helpers/runServer.ts
@@ -1,9 +1,11 @@
 import { type IncomingMessage as Request, type ServerResponse as Response, type Server, createServer } from 'node:http'
 
-export const runServer = (func: (req: Request, res: Response) => void): Server => {
-  const s = createServer((req, res) => {
-    func(req, res)
+export const runServer = (func: (req: Request, res: Response) => void | Promise<void>): Server => {
+  return createServer(async (req, res) => {
+    try {
+      await func(req, res)
+    } finally {
+      res.end()
+    }
   })
-
-  return s
 }
--- tests/modules/send.test.ts
+++ tests/modules/send.test.ts
@@ -1,6 +1,5 @@
 import fs from 'node:fs'
 import path from 'node:path'
-import { dirname } from 'dirname-filename-esm'
 import { makeFetch } from 'supertest-fetch'
 import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
@@ -8,36 +7,34 @@ import { App } from '@/packages/app/src'
 import { json, send, sendFile, sendStatus, status } from '@/packages/send/src'
 import { runServer } from '@/test_helpers/runServer'
 
-const __dirname = dirname(import.meta)
-
 describe('json(body)', () => {
   it('should send a json-stringified reply when an object is passed', async () => {
-    const app = runServer((_, res) => json(res)({ hello: 'world' }))
+    const app = runServer((_, res) => void json(res)({ hello: 'world' }))
 
     await makeFetch(app)('/').expect({ hello: 'world' })
   })
   it('should set a content-type header properly', async () => {
-    const app = runServer((_, res) => json(res)({ hello: 'world' }))
+    const app = runServer((_, res) => void json(res)({ hello: 'world' }))
 
     await makeFetch(app)('/').expectHeader('content-type', 'application/json')
   })
   it('should send a null reply when an null is passed', async () => {
-    const app = runServer((_, res) => json(res)(null))
+    const app = runServer((_, res) => void json(res)(null))
 
     await makeFetch(app)('/').expect(null)
   })
   it('should be able to respond with booleans', async () => {
-    const app = runServer((_, res) => json(res)(true))
+    const app = runServer((_, res) => void json(res)(true))
 
     await makeFetch(app)('/').expectBody('true')
   })
   it('should be able to respond with numbers', async () => {
-    const app = runServer((_, res) => json(res)(123))
+    const app = runServer((_, res) => void json(res)(123))
 
     await makeFetch(app)('/').expectBody('123')
   })
   it('should be able to respond with strings', async () => {
-    const app = runServer((_, res) => json(res)('hello'))
+    const app = runServer((_, res) => void json(res)('hello'))
 
     await makeFetch(app)('/').expectBody('hello')
   })
@@ -45,39 +42,39 @@ describe('json(body)', () => {
 
 describe('send(body)', () => {
   it('should send a plain text', async () => {
-    const app = runServer((req, res) => send(req, res)('Hello World'))
+    const app = runServer((req, res) => void send(req, res)('Hello World'))
 
     await makeFetch(app)('/').expect('Hello World')
   })
   it('should set HTML content-type header when sending plain text', async () => {
-    const app = runServer((req, res) => send(req, res)('Hello World'))
+    const app = runServer((req, res) => void send(req, res)('Hello World'))
 
     await makeFetch(app)('/').expectHeader('Content-Type', 'text/html; charset=utf-8')
   })
   it('should generate an eTag on a plain text response', async () => {
-    const app = runServer((req, res) => send(req, res)('Hello World'))
+    const app = runServer((req, res) => void send(req, res)('Hello World'))
 
     await makeFetch(app)('/').expectHeader('etag', 'W/"b-Ck1VqNd45QIvq3AZd8XYQLvEhtA"')
   })
   it('should send a JSON response', async () => {
-    const app = runServer((req, res) => send(req, res)({ hello: 'world' }))
+    const app = runServer((req, res) => void send(req, res)({ hello: 'world' }))
 
     await makeFetch(app)('/').expectHeader('Content-Type', 'application/json').expectBody({ hello: 'world' })
   })
   it('should send a buffer', async () => {
-    const app = runServer((req, res) => send(req, res)(Buffer.from('Hello World')))
+    const app = runServer((req, res) => void send(req, res)(Buffer.from('Hello World')))
 
     await makeFetch(app)('/').expect('Hello World')
   })
   it('should send nothing on a HEAD request', async () => {
-    const app = runServer((req, res) => send(req, res)('Hello World'))
+    const app = runServer((req, res) => void send(req, res)('Hello World'))
 
     await makeFetch(app)('/', {
       method: 'HEAD'
     }).expectBody('')
   })
   it('should send nothing if body is empty', async () => {
-    const app = runServer((req, res) => send(req, res)(null))
+    const app = runServer((req, res) => void send(req, res)(null))
 
     await makeFetch(app)('/').expectBody('')
   })
@@ -106,7 +103,7 @@ describe('send(body)', () => {
       .expectHeader('Transfer-Encoding', null)
   })
   it("should set Content-Type to application/octet-stream for buffers if the header hasn't been set before", async () => {
-    const app = runServer((req, res) => send(req, res)(Buffer.from('Hello World', 'utf-8')).end())
+    const app = runServer((req, res) => void send(req, res)(Buffer.from('Hello World', 'utf-8')).end())
 
     await makeFetch(app)('/').expectHeader('Content-Type', 'application/octet-stream')
   })
@@ -132,7 +129,7 @@ describe('send(body)', () => {
 
 describe('status(status)', () => {
   it('sets response status', async () => {
-    const app = runServer((_, res) => status(res)(418).end())
+    const app = runServer((_, res) => void status(res)(418).end())
 
     await makeFetch(app)('/').expectStatus(418)
   })
@@ -151,14 +148,14 @@ describe('status(status)', () => {
 
 describe('sendStatus(status)', () => {
   it(`should send "I'm a teapot" when argument is 418`, async () => {
-    const app = runServer((req, res) => sendStatus(req, res)(418).end())
+    const app = runServer((req, res) => void sendStatus(req, res)(418).end())
 
     await makeFetch(app)('/').expect("I'm a Teapot")
   })
 })
 
 describe('sendFile(path)', () => {
-  const testFilePath = path.resolve(__dirname, 'test.txt')
+  const testFilePath = path.resolve(import.meta.dirname, '..', 'fixtures', 'test.txt')
 
   beforeAll(() => {
     fs.writeFileSync(testFilePath, 'Hello World')
@@ -169,38 +166,32 @@ describe('sendFile(path)', () => {
   })
 
   it('should send the file', async () => {
-    const app = runServer((req, res) => sendFile(req, res)(testFilePath, {}))
+    const app = runServer(async (req, res) => {
+      await sendFile(req, res)(testFilePath, {})
+    })
 
     await makeFetch(app)('/').expect('Hello World')
   })
 
   it('should throw if path is not absolute', async () => {
     const app = runServer(async (req, res) => {
-      try {
-        sendFile(req, res)('../relative/path', {})
-      } catch (err) {
-        expect(err.message).toMatch(/absolute/)
-
-        res.end()
-
-        return
-      }
-
-      throw new Error('Did not throw an error')
+      await expect(sendFile(req, res)('../relative/path', {})).rejects.toThrow(/absolute/)
     })
 
     await makeFetch(app)('/')
   })
   it('should set the Content-Type header based on the filename', async () => {
-    const app = runServer((req, res) => sendFile(req, res)(testFilePath, {}))
+    const app = runServer(async (req, res) => {
+      await sendFile(req, res)(testFilePath, {})
+    })
 
     await makeFetch(app)('/').expectHeader('Content-Type', 'text/plain; charset=utf-8')
   })
   it('should inherit the previously set Content-Type header', async () => {
-    const app = runServer((req, res) => {
+    const app = runServer(async (req, res) => {
       res.setHeader('Content-Type', 'text/markdown')
 
-      sendFile(req, res)(testFilePath, {})
+      await sendFile(req, res)(testFilePath, {})
     })
 
     await makeFetch(app)('/').expectHeader('Content-Type', 'text/markdown')
@@ -209,13 +200,18 @@ describe('sendFile(path)', () => {
     const HEADER_NAME = 'Test-Header'
     const HEADER_VALUE = 'Hello World'
 
-    const app = runServer((req, res) => sendFile(req, res)(testFilePath, { headers: { [HEADER_NAME]: HEADER_VALUE } }))
+    const app = runServer(async (req, res) => {
+      await sendFile(req, res)(testFilePath, { headers: { [HEADER_NAME]: HEADER_VALUE } })
+    })
 
     await makeFetch(app)('/').expectHeader(HEADER_NAME, HEADER_VALUE)
   })
 
   it('should support Range header', async () => {
-    const app = runServer((req, res) => sendFile(req, res)(testFilePath))
+    const app = runServer(async (req, res) => {
+      await sendFile(req, res)(testFilePath)
+    })
+
     await makeFetch(app)('/', {
       headers: {
         Range: 'bytes=0-4'
@@ -227,7 +223,9 @@ describe('sendFile(path)', () => {
       .expect('Hello')
   })
   it('should send 419 if out of range', async () => {
-    const app = runServer((req, res) => sendFile(req, res)(testFilePath))
+    const app = runServer(async (req, res) => {
+      await sendFile(req, res)(testFilePath)
+    })
     await makeFetch(app)('/', {
       headers: {
         Range: 'bytes=0-666'
@@ -237,13 +235,15 @@ describe('sendFile(path)', () => {
       .expectHeader('Content-Range', 'bytes */11')
   })
   it('should set default encoding to UTF-8', async () => {
-    const app = runServer((req, res) => sendFile(req, res)(testFilePath))
+    const app = runServer(async (req, res) => {
+      await sendFile(req, res)(testFilePath)
+    })
     await makeFetch(app)('/').expectStatus(200).expectHeader('Content-Encoding', 'utf-8')
   })
   it('should inherit the previously set status code', async () => {
-    const app = runServer((req, res) => {
+    const app = runServer(async (req, res) => {
       res.statusCode = 418
-      sendFile(req, res)(testFilePath)
+      await sendFile(req, res)(testFilePath)
     })
 
     await makeFetch(app)('/').expectStatus(418)
