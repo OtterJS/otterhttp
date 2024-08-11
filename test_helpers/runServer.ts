bdd3cbb68135ae6d88a001cb380cce8917a59fb6
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-08-11 14:40:37 +0100

    tooling(test): call `req.populate`

--- test_helpers/runServer.ts
+++ test_helpers/runServer.ts
@@ -1,8 +1,9 @@
-import { type IncomingMessage, type Server, type ServerResponse, createServer } from 'node:http'
+import { type Server, type ServerResponse, createServer } from 'node:http'
 import { Request } from '@/packages/app/src'
 
-export const runServer = (func: (req: IncomingMessage, res: ServerResponse) => void | Promise<void>): Server => {
-  const listener = async (req: IncomingMessage, res: ServerResponse) => {
+export const runServer = (func: (req: Request, res: ServerResponse) => void | Promise<void>): Server => {
+  const listener = async (req: Request, res: ServerResponse) => {
+    req.populate({ trust: 0, subdomainOffset: undefined })
     try {
       await func(req, res)
     } finally {
