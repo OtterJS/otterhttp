dcb71706d1e8c205a265f25e8aa45a95f55dc9b2
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-08-11 14:22:12 +0100

    tooling(tests): pass `Request` to `http.createServer`

--- test_helpers/runServer.ts
+++ test_helpers/runServer.ts
@@ -1,11 +1,14 @@
-import { type IncomingMessage as Request, type ServerResponse as Response, type Server, createServer } from 'node:http'
+import { type IncomingMessage, type Server, type ServerResponse, createServer } from 'node:http'
+import { Request } from '@/packages/app/src'
 
-export const runServer = (func: (req: Request, res: Response) => void | Promise<void>): Server => {
-  return createServer(async (req, res) => {
+export const runServer = (func: (req: IncomingMessage, res: ServerResponse) => void | Promise<void>): Server => {
+  const listener = async (req: IncomingMessage, res: ServerResponse) => {
     try {
       await func(req, res)
     } finally {
       res.end()
     }
-  })
+  }
+
+  return createServer({ IncomingMessage: Request }, listener)
 }
