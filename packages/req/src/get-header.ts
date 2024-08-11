f5ac79860ffb802891126a2f583fe6a081e3f9f6
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-08-11 14:37:24 +0100

    fix(req): handle `referrer`

--- packages/req/src/get-header.ts
+++ packages/req/src/get-header.ts
@@ -1,16 +1,20 @@
 import type { IncomingHttpHeaders } from 'node:http'
 import type { HasHeaders } from './types'
 
+type Headers = IncomingHttpHeaders & {
+  referrer: string | undefined
+}
+
 export function getRequestHeader<HeaderName extends string>(
   req: HasHeaders,
   headerName: HeaderName
-): IncomingHttpHeaders[HeaderName] {
+): Headers[HeaderName] {
   const headerNameLowerCase = headerName.toLowerCase()
 
   switch (headerNameLowerCase) {
     case 'referer':
     case 'referrer':
-      return req.headers.referer ?? req.headers.referer
+      return req.headers.referer || (req.headers.referrer as string | undefined)
     default:
       return req.headers[headerNameLowerCase]
   }
