2d081d4a03c543f0a75a7709eee6d0c396ff5c45
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-08-13 12:29:10 +0100

    test(type-is): adjust for API changes

--- tests/modules/type-is.test.ts
+++ tests/modules/type-is.test.ts
@@ -16,44 +16,44 @@ describe('typeIs', () => {
   })
 
   it('should return value if types are empty', () => {
-    expect(typeIs('application/json', ...['application/json'])).toBe('application/json')
+    expect(typeIs('application/json', ['application/json'])).toBe('application/json')
   })
 
   it('should return value if matched type starts with plus', () => {
-    expect(typeIs('application/ld+json', '+json')).toBe('application/ld+json')
+    expect(typeIs('application/ld+json', ['+json'])).toBe('application/ld+json')
   })
 
   it('should return false if there is no match', () => {
-    expect(typeIs('application/ld+json', 'application/javascript')).toBe(false)
+    expect(typeIs('application/ld+json', ['application/javascript'])).toBe(false)
   })
 
   it('should return false if there is no match', () => {
-    expect(typeIs('text/html', 'application/javascript')).toBe(false)
+    expect(typeIs('text/html', ['application/javascript'])).toBe(false)
   })
 
   it('should return matched value for urlencoded shorthand', () => {
-    expect(typeIs('application/x-www-form-urlencoded', 'urlencoded')).toBe('urlencoded')
+    expect(typeIs('application/x-www-form-urlencoded', ['urlencoded'])).toBe('urlencoded')
   })
 
   it('should return matched value for urlencoded shorthand', () => {
-    expect(typeIs('multipart/form-data', 'multipart')).toBe('multipart')
+    expect(typeIs('multipart/form-data', ['multipart'])).toBe('multipart')
   })
 
   it.each(['', false, null, undefined])(
     "should return false when matching against a falsy value '%s'",
     (value: unknown) => {
-      expect(typeIs('multipart/form-data', value as any)).toBe(false)
+      expect(typeIs('multipart/form-data', [value as any])).toBe(false)
     }
   )
 
   it('should return false if expected type has wrong format', () => {
-    expect(typeIs('multipart/form-data', 'application/javascript/wrong')).toBe(false)
+    expect(typeIs('multipart/form-data', ['application/javascript/wrong'])).toBe(false)
   })
   it('should return false if the input is not a string', () => {
     const value: Record<number, string> = { 1: 'test' }
-    expect(typeIs(value as unknown as string)).toBe(false)
+    expect(typeIs(value as any)).toBe(false)
   })
   it('should return the same type as input if the type is not normalized', () => {
-    expect(typeIs('text/html', 'file.html')).toBe('file.html')
+    expect(typeIs('text/html', ['file.html'])).toBe('file.html')
   })
 })
