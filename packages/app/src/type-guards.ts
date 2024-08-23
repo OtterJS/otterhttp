188d099625797351002e3f74cc3e87082132e5c6
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-08-23 15:34:56 +0100

    chore(app): add some useful type-guards

--- packages/app/src/type-guards.ts
+++ packages/app/src/type-guards.ts
@@ -2,6 +2,14 @@ export const isString = (something: unknown): something is string => {
   return typeof something === 'string' || something instanceof String
 }
 
+export const isNumber = (something: unknown): something is number => {
+  return typeof something === 'number' || something instanceof Number
+}
+
+export const isIndexer = (something: unknown): something is string | number => {
+  return isString(something) || isNumber(something)
+}
+
 export const isStringArray = (something: unknown): something is string[] => {
   if (typeof something !== 'object') return false
   if (!Array.isArray(something)) return false
